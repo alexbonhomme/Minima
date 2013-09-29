4f3160828b504316861a43d277a15a165946c05e
blckshrk <alexand.bonhom@gmail.com>
2013-09-29 20:56:44 +0200

    Structure and color palette choices, first page sketched

---
+++ README.md
@@ -0,0 +1,4 @@
+# Minima
+Fork of Cool Kitten (https://github.com/jalxob/cool-kitten) by @blckshrk
+
+
---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function ($) {
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    htmlBody = $('html,body');
+
+	slide.bind('inview', function(event, isInView) {
+		dataSlide = $(this).attr('data-slide');
+
+		if (isInView) {
+			$('.navigation li[data-slide="' + dataSlide + '"]').addClass('active');
+		} else {
+			$('.navigation li[data-slide="' + dataSlide + '"]').removeClass('active');
+		}
+	});
+
+    function goToByScroll(dataSlide) {
+    	var position = $('.slide[data-slide="' + dataSlide + '"]').offset().top;
+        
+        htmlBody.animate({
+            scrollTop: position
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataSlide = $(this).attr('data-slide');
+        goToByScroll(dataSlide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataSlide = $(this).attr('data-slide');
+        goToByScroll(dataSlide);
+    });
+
+});
